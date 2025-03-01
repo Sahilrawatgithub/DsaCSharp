a88cd526e316dd56a53b71757bbccc5c0f3e6094
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-03-01 14:01:26 +0530

    Added arrays branch

---
+++ MoveZeroes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_core
+{
+    internal class MoveZeroes
+    {
+        public void Start()
+        {
+           
+            Console.WriteLine("Enter the size of array : ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the elements of array and press enter after each insertion");
+            for(int i = 0; i < n; i++)
+            {
+                arr[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Here is the array before adjusting zeroes");
+           
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] == 0 && arr[j]!=0)
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        break;
+                    }
+                    
+                }
+            }
+            
+            Console.WriteLine("Here is array after putting zeroes at end");
+            
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is the Dev branch, check other branches for code");
+            Console.WriteLine("I have many array problems, please adhere to following instructions for a smooth experience.");
+            Console.WriteLine("Press 1 if you want to Put zeroes at end");
+            
+            Console.WriteLine("Press 6 if you want to exit");
+            Console.WriteLine("\n");
+
+            bool Flag = true;
+            while (Flag)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        MoveZeroes moveZeroes = new MoveZeroes();
+                        moveZeroes.Start();
+                        break;
+
+                    case "6":
+                        Console.WriteLine("Exiting");
+                        Flag = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input, please follow instructions.");
+                        break;
+                }
+            }
         }
     }
 }
